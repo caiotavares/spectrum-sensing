f01ad35f632d4c8fdeb9b34f2ea2eba0a37526cc
Caio Tavares <caio.tavares11@gmail.com>
2017-04-18 16:43:56 -0300

    Simplifying the argument list.

--- lib/gaussianNoise.m
+++ lib/gaussianNoise.m
@@ -1,4 +1,4 @@
-function [n] = gaussianNoise(N,samples,noisePower)
+function [n] = gaussianNoise(samples,noisePower)
 
 % Returns the Gaussian Noise with zero mean and unitary variance
 % 
@@ -7,6 +7,7 @@ function [n] = gaussianNoise(N,samples,noisePower)
 % samples - Number of noise samples 
 % noisePower - Noise power for each SU
 
+N = size(noisePower,1);
 n = zeros(N,samples);
 
 for i=1:N
