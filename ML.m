e0919fce0f34a50efef75d53106deb41eb67afc7
Caio Tavares <caio.tavares11@gmail.com>
2017-05-10 11:52:52 -0300

    Obtained the ROC curve for MLP in R. Included a way to call the R file from Matlab and load the output.

--- ML.m
+++ ML.m
@@ -1,23 +1,26 @@
 %% Training Methodology (not operational)
 
-trainingPercent = 0.7;
-truePositives = find(A==1);
-trueNegatives = find(A==0);
-positiveTrainingIndexes = truePositives(randperm(length(truePositives),round(length(truePositives)*trainingPercent)));
-negativeTrainingIndexes = trueNegatives(randperm(length(trueNegatives),round(length(trueNegatives)*trainingPercent)));
-trainingIndexes = sort([positiveTrainingIndexes;negativeTrainingIndexes]);
-testIndexes = setdiff(1:size(X,1),trainingIndexes)';
+system('Rscript R/ML.r')
+load('MLP')
 
-X_training = array2table([X(trainingIndexes,:) A(trainingIndexes)]);
-X_test = array2table([X(testIndexes,:) A(testIndexes)]);
-
-names = cell(1,size(scenario.SU,1));
-for i=1:size(scenario.SU,1)
-   names{i} = ['SU_' num2str(i)];
-end
-names{end+1} = 'Channel_Status';
-
-X_training.Properties.VariableNames = names;
-X_test.Properties.VariableNames = names;
+% trainingPercent = 0.7;
+% truePositives = find(A==1);
+% trueNegatives = find(A==0);
+% positiveTrainingIndexes = truePositives(randperm(length(truePositives),round(length(truePositives)*trainingPercent)));
+% negativeTrainingIndexes = trueNegatives(randperm(length(trueNegatives),round(length(trueNegatives)*trainingPercent)));
+% trainingIndexes = sort([positiveTrainingIndexes;negativeTrainingIndexes]);
+% testIndexes = setdiff(1:size(X,1),trainingIndexes)';
+% 
+% X_training = array2table([X(trainingIndexes,:) A(trainingIndexes)]);
+% X_test = array2table([X(testIndexes,:) A(testIndexes)]);
+% 
+% names = cell(1,size(scenario.SU,1));
+% for i=1:size(scenario.SU,1)
+%    names{i} = ['SU_' num2str(i)];
+% end
+% names{end+1} = 'Channel_Status';
+% 
+% X_training.Properties.VariableNames = names;
+% X_test.Properties.VariableNames = names;
 
 % GM = fitgmdist(X_training,2,'Start',startObj,'Options',options,'CovarianceType','diagonal');
--- R/ML.r
+++ R/ML.r
@@ -1,4 +1,8 @@
+library(R.matlab)
 library(caret)
+library(pROC)
+
+setwd("C:/Users/Caio Tavares/Repositories/mestrado/Spectrum Sensing/R")
 
 accuracy <- function(confusion)
 {
@@ -12,15 +16,23 @@ X$Status <- factor(X$Status, levels = c(0,1))
 
 training <- 0.7
 X.train.len <- round(training*nrow(X))
-
 X.train <-  X[1:X.train.len,]
 X.test <- X[(X.train.len+1):nrow(X),]
 
-MLP <- caret::train(X.train[,-ncol(X.train)], X.train$Status, trControl = trainControl(method = "cv", number = 10), 
-             method = "mlp", tuneLength = 1)
+MLP <- caret::train(X.train[,-ncol(X.train)], X.train$Status, 
+                    trControl = trainControl(method = "cv", number = 10), method = "mlp", tuneLength = 1)
+prediction <- predict(MLP$finalModel, X.test[,-ncol(X.test)])
 
-result <- predict(MLP$finalModel, X.test[,-ncol(X.test)])
-# The max.col methods can be replaced in order to control the Pfa/Pd trade-off
-result <- matrix(max.col(result, ties.method = "first")-1, ncol = 1)
-confMatrix <- table(actual = X.test$Status, predicted = result)
-MLP.accuracy = accuracy(confMatrix)
+threshold <- (1000:1)/1000
+Pd <- c()
+Pfa <- c()
+index <- 1
+for(t in threshold)
+{
+  result <- factor((prediction[,2]>=t)*1,levels = c(0,1))
+  confMatrix <- table(actual = X.test$Status, predicted = result)
+  Pd[index] <- confMatrix[2,2]/sum(X.test$Status==1)
+  Pfa[index] <- confMatrix[1,2]/sum(X.test$Status==0)
+  index <- index+1
+}
+writeMat(Pd = Pd, Pfa = Pfa, con = "../MLP.mat")
